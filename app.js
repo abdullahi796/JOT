79a3950c60f258e9af5286f9c02c75f14f99603c
Abdullahi0988 <abdullahimahamed0987@gmail.com>
2015-04-25 04:53:12 +0000

    Added ranking functions

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 (function() {
   var app = angular.module('jokeApp', []);
   app.controller('jokeController',function () {
-    this.header = 'The who';
     this.alertClick = function() {
         alert('Click');
     }
@@ -16,25 +15,33 @@
               text: 'She wanted to stretch her legs.',
               author:'Jackie Marthart ',
               likes: 3,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: 'To prove to the possum it could actually be done!',
               author:'Larry Ball',
               likes: 13,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: 'She was afraid someone would caesar!',
               author:'Ibrahim Mahamed',
               likes: 5,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: 'She wanted to lay it on the line.',
               author:'Kate Hammer',
               likes: 10,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
         ],
         num: 1
@@ -50,13 +57,17 @@
               text: 'The horse says I m clinically depressed',
               author:'Jackie Marthart ',
               likes: 5,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: 'The horse, incapable of speaking, proceeds to run around the bar, breaking several items in the process.',
               author:'Larry Ball',
               likes: 25,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
         ],
         num: 2
@@ -71,19 +82,25 @@
               text: 'For absenteeism and theft of property.',
               author:'Jackie Marthart ',
               likes: 1,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: "For throwing out all the W's",
               author:'Larry Ball',
               likes: 45,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
             {
               text: 'For trying to alphabetize them',
               author:'Ibrahim Mahamed',
               likes: 10,          
-              voted: "n/a"
+              voted: "n/a",
+              colorUp:'na',
+              colorDown:'na'
             },
         ],
         num: 3
@@ -93,20 +110,34 @@
       if(punchline.voted === "n/a"){
         punchline.voted = 'up';
         punchline.likes +=1;
+        punchline.colorUp='green'
       }
       else if(punchline.voted === 'down'){
         punchline.voted = 'n/a';
         punchline.likes +=1;
+        punchline.colorDown='na'
+      }
+      else if(punchline.voted === 'up'){
+        punchline.voted = 'n/a';
+        punchline.likes -=1;
+        punchline.colorUp='na'
       }
     };
     this.voteDown = function(punchline) {
       if(punchline.voted === "n/a"){
         punchline.voted = 'down';
         punchline.likes -=1;
+        punchline.colorDown='red'
       }
       else if(punchline.voted === 'up'){
         punchline.voted = 'n/a';
         punchline.likes -=1;
+        punchline.colorUp='na'
+      }
+      else if(punchline.voted === 'down'){
+        punchline.voted = 'n/a';
+        punchline.likes +=1;
+        punchline.colorDown='na'
       }
     };    
   });
