906064771df8b59a81164e4367d5aefdc997e97a
Abdullahi0988 <abdullahimahamed0987@gmail.com>
2015-04-25 04:19:08 +0000

    Finished Form/and working on rank button

--- app.js
+++ app.js
@@ -9,24 +9,32 @@
         {
         setup:{
             text: 'Why did the chicken run across the road?',
-            author: 'Abdullahi Mahamed'
+            author: 'Abdullahi Mahamed',
         },
         punchline: [
             {
               text: 'She wanted to stretch her legs.',
               author:'Jackie Marthart ',
+              likes: 3,          
+              voted: "n/a"
             },
             {
               text: 'To prove to the possum it could actually be done!',
               author:'Larry Ball',
+              likes: 13,          
+              voted: "n/a"
             },
             {
               text: 'She was afraid someone would caesar!',
               author:'Ibrahim Mahamed',
+              likes: 5,          
+              voted: "n/a"
             },
             {
               text: 'She wanted to lay it on the line.',
               author:'Kate Hammer',
+              likes: 10,          
+              voted: "n/a"
             },
         ],
         num: 1
@@ -41,10 +49,14 @@
             {
               text: 'The horse says I m clinically depressed',
               author:'Jackie Marthart ',
+              likes: 5,          
+              voted: "n/a"
             },
             {
               text: 'The horse, incapable of speaking, proceeds to run around the bar, breaking several items in the process.',
               author:'Larry Ball',
+              likes: 25,          
+              voted: "n/a"
             },
         ],
         num: 2
@@ -58,19 +70,45 @@
             {
               text: 'For absenteeism and theft of property.',
               author:'Jackie Marthart ',
+              likes: 1,          
+              voted: "n/a"
             },
             {
               text: "For throwing out all the W's",
               author:'Larry Ball',
+              likes: 45,          
+              voted: "n/a"
             },
             {
               text: 'For trying to alphabetize them',
               author:'Ibrahim Mahamed',
+              likes: 10,          
+              voted: "n/a"
             },
         ],
         num: 3
     }    
     ];
+    this.voteUp = function(punchline) {
+      if(punchline.voted === "n/a"){
+        punchline.voted = 'up';
+        punchline.likes +=1;
+      }
+      else if(punchline.voted === 'down'){
+        punchline.voted = 'n/a';
+        punchline.likes +=1;
+      }
+    };
+    this.voteDown = function(punchline) {
+      if(punchline.voted === "n/a"){
+        punchline.voted = 'down';
+        punchline.likes -=1;
+      }
+      else if(punchline.voted === 'up'){
+        punchline.voted = 'n/a';
+        punchline.likes -=1;
+      }
+    };    
   });
   app.controller('panelController',function() {
         this.tab = 1;
@@ -90,6 +128,7 @@
       this.addPunchline = function (joke) {
         joke.punchline.push(this.userLine);
         this.userLine = {};
+        this.userLine.author = "anonymous"
       };
   });
   
