4b993e8756e80472789d38afa695340632424878
Abdullahi0988 <abdullahimahamed0987@gmail.com>
2015-04-25 05:15:28 +0000

    Finished Ranking System

--- app.js
+++ app.js
@@ -156,10 +156,18 @@
   app.controller('commentController',function() {
       this.userLine = {};
       this.userLine.author = "anonymous"
+      this.userLine.likes = 0;       
+      this.userLine.voted = "n/a";
+      this.userLine.colorUp = 'na';
+      this.userLine.colorDown = 'na';
       this.addPunchline = function (joke) {
         joke.punchline.push(this.userLine);
         this.userLine = {};
         this.userLine.author = "anonymous"
+        this.likes = 0;         
+        this.voted = "n/a";
+        this.colorUp = 'na';
+        this.colorDown = 'na';      
       };
   });
   
